009c84235e9919498408f4fdda142217d57debad
Daniel Lagos <lagos.d@gmail.com>
2024-02-19 10:28:08 -0800

    worked taqueria.py assitnmen and is complete.

--- lecture_3/problem_set/taqueria.py
+++ lecture_3/problem_set/taqueria.py
@@ -1,11 +1,23 @@
 def main():
-    item = take_order()
-    calculate_total(item)
+    # set up total_price variable as float.  
+    total_price = 0.0
+        
+    while True:
+        # try to take order, if EOFError or a Keyboard innterupt print total price and break loop
+        try: 
+            price = take_order()
+        except (EOFError, KeyboardInterrupt):
+            print(f"Total: ${total_price}")
+            break
+           
+        # if price contains invalid input, print total, otherwise add price to total price. 
+        if price == None:
+            print(f"Total: ${total_price}")
+        else:
+            total_price += price
+            print(f"Total: ${total_price}")
     
 def take_order():
-    return input("Item: ")
-
-def calculate_total(item):
     menu = {
         "Baja Taco": 4.25,
         "Burrito": 7.50,
@@ -17,8 +29,17 @@ def calculate_total(item):
         "Taco": 3.00,
         "Tortilla Salad": 8.00
         }
+        
+    # convert user input to lower case.  For case insensitive look up.
+    item_ordered = input("Item: ").lower()
     
-    print(item)
-    print(f"Price: {menu[item]}")
+    # recreate menu dictionary with lower case keys.  Also for case insensitive look up.
+    lowercase_dict = {key.lower(): value for key, value in menu.items()}
+    
+    # look up price by item inputed by user.
+    menu_item_price = lowercase_dict.get(item_ordered)
+    
+    # return price of item
+    return menu_item_price
 
 main()
