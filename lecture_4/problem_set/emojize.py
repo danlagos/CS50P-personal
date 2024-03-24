2ba303c81d88de469a4565f0370323acd4b2bfa1
Daniel Lagos <lagos.d@gmail.com>
2024-03-24 11:33:49 -0700

    finish emojize.py

---
+++ lecture_4/problem_set/emojize.py
@@ -0,0 +1,17 @@
+import emoji
+
+def main():
+    """
+    call take_input() and place input in variable
+    produce output with emoji, if emoji command is present
+    """
+    user_input = take_input()
+    print("Output: " + emoji.emojize(user_input, language='alias'))
+
+def take_input():
+    user_input = input("Input: ")
+    return user_input    
+
+if __name__ == "__main__":
+    main()
+   
