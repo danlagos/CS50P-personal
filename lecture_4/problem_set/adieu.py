fdf47c50bd7b44fc07b0a1b6cfa3e0e2fe010510
Daniel Lagos <lagos.d@gmail.com>
2024-03-27 09:55:46 -0700

    pseudo code complete.

--- lecture_4/problem_set/adieu.py
+++ lecture_4/problem_set/adieu.py
@@ -1,13 +1,28 @@
 import inflect
 
 def main ():
+    """
+    calls take_input() and stores in list_of_names
+    call format_output(list_of_names) update in list_of_names
+    print farewell message:  With the formatted list_of_names, prepare the final message to be printed to the console. Ensure it starts with "Adieu, adieu, to " followed by the formatted names
+    """
     pass
 
 def take_input():
     """
-    Create a function to prompt the user for names and store them in a list. 
-    This function should   handle the EOFError to break out of the loop when Ctrl-D is pressed.
-    return list
+    Prompt the user for names in a loop.
+    Handle EOFError to break the loop when Ctrl-D is pressed.
+    Store and return the names in a list.
     """
     pass
 
+def format_output(list_of_names):
+    """ 
+    consider using inflect library
+    takes the list of names as input 
+    returns a formatted string according to the rules given (using commas and "and" appropriately).
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
