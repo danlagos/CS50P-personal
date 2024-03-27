d211245183df51ec46af91b8bcf0b17a0c30cf3d
Daniel Lagos <lagos.d@gmail.com>
2024-03-27 09:37:30 -0700

    starting pseudo for adieu.py.

---
+++ lecture_4/problem_set/adieu.py
@@ -0,0 +1,13 @@
+import inflect
+
+def main ():
+    pass
+
+def take_input():
+    """
+    Create a function to prompt the user for names and store them in a list. 
+    This function should   handle the EOFError to break out of the loop when Ctrl-D is pressed.
+    return list
+    """
+    pass
+
