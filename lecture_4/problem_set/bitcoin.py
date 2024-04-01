b48d8e72964546f3b9806af4694f290942b73cda
Daniel Lagos <lagos.d@gmail.com>
2024-04-01 12:36:12 -0700

    finished validate_cli_arg function.

--- lecture_4/problem_set/bitcoin.py
+++ lecture_4/problem_set/bitcoin.py
@@ -22,7 +22,14 @@ def validate_cli_arg():
     Returns:
     - The number of Bitcoins as a float if validation succeeds.
     """
-    pass
+    if len(sys.argv) != 2:
+        sys.exit("Missing command-line argument")
+        
+    try:
+        number_of_bitcoin = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+    return number_of_bitcoin
 
 def fetch_bitcoin_data():
     """ 
