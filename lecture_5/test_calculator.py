c60b5ad6e19622f5ba618df766f402b76a6ee4bc
Daniel Lagos <lagos.d@gmail.com>
2024-04-02 11:36:48 -0700

    refactored for pytest usage.

--- lecture_5/test_calculator.py
+++ lecture_5/test_calculator.py
@@ -1,29 +1,8 @@
 from calculator import square
 
-def main():
-    test_square()
-
 def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 Squared was not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9")
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print("-2 Squared was not 4")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print("-3 squared was not 9")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("0 squared was not 0")
-        
-if __name__ == "__main__":
-    main()
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
