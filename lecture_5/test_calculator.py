de209c772fd6360bb4d5fd0198d2d46508e1eb60
Daniel Lagos <lagos.d@gmail.com>
2024-04-02 11:30:53 -0700

    saving test_calculator.  about to learn about pytest.  This file will require reformating.

--- lecture_5/test_calculator.py
+++ lecture_5/test_calculator.py
@@ -12,6 +12,18 @@ def test_square():
         assert square(3) == 9
     except AssertionError:
         print("3 squared was not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 Squared was not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared was not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared was not 0")
         
 if __name__ == "__main__":
     main()
