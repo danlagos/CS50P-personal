8ea18cf4bc67623f61bb8a3ca3f5f170da1c0bde
Daniel Lagos <lagos.d@gmail.com>
2024-04-02 11:25:18 -0700

    usgae of try/except block.

--- lecture_5/test_calculator.py
+++ lecture_5/test_calculator.py
@@ -4,8 +4,14 @@ def main():
     test_square()
 
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
-
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 Squared was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared was not 9")
+        
 if __name__ == "__main__":
     main()
