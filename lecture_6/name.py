b81cae733e7c5c23ac5ed7385be455ae7c49102a
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-04-14 18:15:39 +0000

    class is about to teach how to refactor from current state of code.

--- lecture_6/name.py
+++ lecture_6/name.py
@@ -1,1 +1,11 @@
+
+
 name = input("What is your name? ")
+
+# this is the programing equivilant of someong double clicking on a file, editing, and saving it.
+# w opens the file every time and writes to it. If the file does not exist, it will create it.
+# appending to a file is done with "a"
+# file = open("names.txt", "w")
+file = open("names.txt", "a")
+file.write(f"{name}\n")
+file.close()
