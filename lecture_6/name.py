f120fbba7dbeac0306f47179c038bec011ad3883
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-04-14 18:16:54 +0000

    learned the with keyword.

--- lecture_6/name.py
+++ lecture_6/name.py
@@ -6,6 +6,6 @@ name = input("What is your name? ")
 # w opens the file every time and writes to it. If the file does not exist, it will create it.
 # appending to a file is done with "a"
 # file = open("names.txt", "w")
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
