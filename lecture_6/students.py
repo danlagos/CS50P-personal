1ed6ca020e31257ae28bfca7535f2bb00548b2a1
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-04-18 19:07:03 +0000

    intorduced dictionary reader.

--- lecture_6/students.py
+++ lecture_6/students.py
@@ -1,11 +1,11 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name , house = line.strip().split(",")
-        student = {"name" : name, "house" : house}
-        students.append(student)
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name" : row["name"], "home" : row["home"]})
 
-
-for student in sorted(students, key=lambda student : student["house"], reverse=True):
-    print(f"{student['name']} is in {student['house']}")
+for student in sorted(students, key=lambda student : student["name"], reverse=True):
+    print(f"{student['name']} is from {student['home']}")
