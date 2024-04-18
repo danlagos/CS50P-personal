0665009fc9e28af74a7514a340960ae5c50d412b
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-04-18 18:37:48 +0000

    mor work with csv files and dictionaries.

--- lecture_6/students.py
+++ lecture_6/students.py
@@ -1,5 +1,16 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        name, house = line.strip().split(",")
-        print(f"{name} is in {house}")
+        name , house = line.strip().split(",")
+        student = {"name" : name, "house" : house}
+        students.append(student)
 
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students, key=get_name, reverse=True):
+    print(f"{student['name']} is in {student['house']}")
