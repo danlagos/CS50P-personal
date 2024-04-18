09b3af42c3505dd29bde9bb002dc6ca43cdbc99d
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-04-18 19:18:22 +0000

    writing to csv files.

--- lecture_6/students.py
+++ lecture_6/students.py
@@ -1,11 +1,8 @@
 import csv
 
-students = []
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name" : row["name"], "home" : row["home"]})
-
-for student in sorted(students, key=lambda student : student["name"], reverse=True):
-    print(f"{student['name']} is from {student['home']}")
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames = ["name", "home"])
+    writer.writerow({"name" : name, "home" : home})
