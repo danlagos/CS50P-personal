138cf1d41af88383526b8ff7fc837db8fee05c05
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-04-22 18:18:45 +0000

    start pizza.py.

--- lecture_6/problem_set/lines.py
+++ lecture_6/problem_set/lines.py
@@ -69,4 +69,5 @@ def process_file(file_path):
                 valid_lines += 1
     return valid_lines
 
-
+if __name__ == "__main__":
+    main()
---
+++ lecture_6/problem_set/pizza.py
@@ -0,0 +1,12 @@
+def main ():
+    """
+    entry point
+    handles main logic 
+    """
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
